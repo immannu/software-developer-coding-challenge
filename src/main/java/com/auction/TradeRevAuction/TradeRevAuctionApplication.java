dc4f9eb7af1912b41b3a2acd1f2cd245022ecb0b
anil <anil.appukuttan@clover.com>
2020-08-24 14:02:26 -0700

    initial commit

---
+++ src/main/java/com/auction/TradeRevAuction/TradeRevAuctionApplication.java
@@ -0,0 +1,13 @@
+package com.auction.TradeRevAuction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeRevAuctionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradeRevAuctionApplication.class, args);
+	}
+
+}
