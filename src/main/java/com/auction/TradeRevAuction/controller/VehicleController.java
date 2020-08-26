99f8cf68fb93c112d05e4e23a8cdf59dcd890bbb
Immannu <>
2020-08-26 16:14:59 -0700

    vehicle controller methods

--- src/main/java/com/auction/TradeRevAuction/controller/VehicleController.java
+++ src/main/java/com/auction/TradeRevAuction/controller/VehicleController.java
@@ -1,15 +1,12 @@
 package com.auction.TradeRevAuction.controller;
 
-import com.auction.TradeRevAuction.model.Account;
 import com.auction.TradeRevAuction.model.Vehicle;
 import com.auction.TradeRevAuction.service.VehicleService;
 import javassist.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,35 +19,26 @@ public class VehicleController {
   @Autowired
   private VehicleService vehicleService;
 
-  @GetMapping("/accounts/{id}")
-  public List<Account> getAllVehicles() {
+  @GetMapping("/accounts/{acId}/vehicle")
+  public List<Vehicle> getAllVehicles(@PathVariable(name = "acId") int acId) throws NotFoundException {
 
-    List<Vehicle> vehicles = vehicleService.
+    List<Vehicle> vehicles = vehicleService.getAllVehicle(acId);
 
-    return accounts;
+    return vehicles;
   }
 
-  @GetMapping("/accounts/{id}")
-  public Account getAccount(@PathVariable(name = "id") int id) throws NotFoundException {
+  @GetMapping("/accounts/{acId}/vehicle/{vId}")
+  public Vehicle getVehicle(@PathVariable(name = "acId") int acId, @PathVariable(name = "vId") int vId) throws NotFoundException {
 
-    return accountService.get(id);
+    Vehicle vehicle = vehicleService.getVehicle(acId, vId);
+
+    return vehicle;
   }
 
-  @PostMapping ("/accounts")
-  public Account createAccount(@RequestBody @Valid Account account) {
+  @PostMapping ("/accounts/{acId}/vehicle")
+  public Vehicle createAccount(@PathVariable(name = "acId") int acId, @RequestBody @Valid Vehicle vehicle) throws NotFoundException {
 
-    return accountService.create(account);
+    return vehicleService.addVehicle(acId, vehicle);
   }
 
-  @PutMapping("/accounts/{id}")
-  public Account updateAccount(@PathVariable(name = "id") int id,@RequestBody @Valid Account account) throws NotFoundException{
-
-    return accountService.update(account);
-  }
-
-  @DeleteMapping("/accounts/{id}")
-  public void deleteAccount(@PathVariable(name = "id") int id) throws NotFoundException{
-
-    accountService.delete(id);
-  }
 }
